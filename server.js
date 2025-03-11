7b5835e2cf83f8bd2dc3a364d94d8ce39baf36bf
CyberdyneSys <60982671+CyberdyneSys@users.noreply.github.com>
2025-03-11 12:23:49 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,19 +1,32 @@
 const WebSocket = require('ws');
+
+// Configura un servidor WebSocket
 const wss = new WebSocket.Server({ port: 8080 });
 
+let currentData = {
+    team1: "Equipo 1",
+    team2: "Equipo 2",
+    score1: 0,
+    score2: 0,
+    timer: "00:00"
+};
+
+// Maneja las conexiones
 wss.on('connection', (ws) => {
-    console.log('Cliente conectado.');
+    console.log("Nuevo cliente conectado");
+    ws.send(JSON.stringify(currentData)); // Enviar datos iniciales al cliente
 
     ws.on('message', (message) => {
-        // Enviar datos a todos los clientes conectados
+        currentData = JSON.parse(message);
+        // Notifica a todos los clientes conectados
         wss.clients.forEach((client) => {
             if (client.readyState === WebSocket.OPEN) {
-                client.send(message);
+                client.send(JSON.stringify(currentData));
             }
         });
     });
 
     ws.on('close', () => {
-        console.log('Cliente desconectado.');
+        console.log("Cliente desconectado");
     });
 });
